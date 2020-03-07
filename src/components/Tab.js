dbb99a67e2b324bb95f9f9f169327e0892602798
Tyler Benning <tbenning27@gmail.com>
2020-03-07 18:13:47 -0500

    clean up code

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -8,33 +8,6 @@ const StyledTab = styled.li`
   margin-bottom: -2px;
   z-index: 100;
 `
-
-const StyledButton = styled.button`
-    border: none;
-    display: inline-block;
-    text-decoration: none;
-    cursor: pointer;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    background: ${colors.ink20};
-    color: ${colors.ink80};
-    border-radius: 20px;
-    font-weight: 600;
-    padding: 12px 20px;
-    cursor: pointer;
-    transition: ease-in-out 0.2s;
-    &:hover {
-        background: ${colors.ink20};
-    }
-    &.active, .active:hover {
-        background: ${colors.brandLight};
-        color: ${colors.teal90};
-    }
-    &:focus {
-        outline: none;
-        box-shadow: inset 0 0 0 2px #004a54;
-    }
-`
 const StyledButtonTwo = styled.button`
     border: none;
     display: inline-block;
